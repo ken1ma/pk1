abb71b6cf4162b513febaf59beae881cf60cae6c
Kenichi Masuko <kenichi@sysart.jp>
2018-12-03 00:12:26 +0900

    Better source layout

--- server/src/ServerConf.scala
+++ server/src/ServerConf.scala
@@ -11,9 +11,17 @@ import org.log4s.getLogger
 object ServerConf {
 	val log = getLogger
 
+	def errorFatal(message: String, ex: Throwable) = { // the return type is Nothing
+		log.error(s"$message: $ex") // don't output the stack trace on the error level
+		log.debug(ex)(s"$message")
+		sys.exit(254) // 255 for command line argument error
+	}
+
 	val conf = try { ConfigFactory.load } catch {
 		case ex: ConfigException => errorFatal("failed to parse the conf", ex)
 	}
+
+	protected def getTopLevel[A](name: String)(implicit decoder: Decoder[A]) = conf.as[A](name).valueOr(errorFatal(s"failed to decode conf: $name", _))
 
 	val https = getTopLevel[Https]("https")
 	case class Https(host: String, port: Int, ssl: Ssl)
@@ -21,12 +29,4 @@ object ServerConf {
 
 	val http = getTopLevel[Http]("http")
 	case class Http(host: String, port: Int)
-
-	protected def getTopLevel[A](name: String)(implicit decoder: Decoder[A]) = conf.as[A](name).valueOr(errorFatal(s"failed to decode conf: $name", _))
-
-	def errorFatal(message: String, ex: Throwable) = { // the return type is Nothing
-		log.error(s"$message: $ex") // don't output the stack trace on the error level
-		log.debug(ex)(s"$message")
-		sys.exit(254) // 255 for command line argument error
-	}
 }
